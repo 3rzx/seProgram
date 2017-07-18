005795cc8d0d3d511d7e73b04499c126efc1ef45
root <root@localhost.localdomain>
2017-07-18 13:33:12 +0800

    add socket part to pp.py

--- pp.py
+++ pp.py
@@ -1,5 +1,8 @@
 import sys
+import os
+import socket
 import time
+
 def funt():
   print 'Protected file content:\n'
   with open('pFile','r') as f:
@@ -11,4 +14,14 @@ def funt():
 	print x
 	time.sleep(1)
 
-funt()
+if __name__ == "__main__":
+  HOST = 'localhost'
+  PORT = 9000
+  s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  s.connect((HOST,PORT))
+  s.send('pid {}'.format(os.getpid()))
+  funt()
+  s.close()
+
+
+
