b9018e95d25a2062e5bade64f84238b63c03247a
root <root@localhost.localdomain>
2017-07-18 13:06:48 +0800

    delete not use code

--- pp.py
+++ pp.py
@@ -1,16 +1,14 @@
 import sys
 import time
-from procname import setprocname
 def funt():
-  with open('whiteList','r') as f:
+  print 'Protected file content:\n'
+  with open('pFile','r') as f:
     for line in f:
       print line	
   f.closed
+  print '===end===\ncount down start'
   for x in range(0,100):
 	print x
 	time.sleep(1)
 
-#setproctitle('usb process')
-#os.path.basename(__file_
-#setprocname('usb_process')
 funt()
